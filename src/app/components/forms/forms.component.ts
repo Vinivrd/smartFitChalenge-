62617f4654bdfd93f0e3bed149d8d39a99b37b20
viniciuscmbr <viniciuscmbrr@gmail.com>
2024-09-02 14:57:44 -0300

    commit is okay

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from 'src/app/services/get-units.service';
+import { Location } from "../../types/location.interface"
 
 @Component({
   selector: 'app-forms',
@@ -8,17 +9,21 @@ import { GetUnitsService } from 'src/app/services/get-units.service';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
-  results = [];
+  results:Location[] = [];
   formGroup!: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) { }
 
   ngOnInit(): void {
-    this.unitService.getAllUnits().subscribe(data => console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false
-    })
+    });
+
+    this.unitService.getAllUnits().subscribe(data => {
+      this.results = data.locations;
+    });
+
   }
 
   onSubmit(): void {
